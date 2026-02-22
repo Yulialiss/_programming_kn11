5ac34827773566be041b539fb82d30f09a615133
Vadym@2216@2413 <www.svyrydiuk.vadym@gmail.com>
2026-02-22 19:23:33 +0200

    Add files via upload

---
+++ Svyrydiuk_Vadym/lab3/src/main2.cpp
@@ -0,0 +1,24 @@
+﻿#include <iostream>
+#include <cmath>
+
+using namespace std;
+
+int main() {
+	double P = 1;
+	double a;
+	int n;
+
+	cout << "Enter a: ";
+	cin >> a;
+	cout << "Enter n: ";
+	cin >> n;
+
+	for (int x = 0; x < n; x += 1) {
+		P *= (a + x);
+	}
+
+	cout << "P = " << P;
+
+	return 0;
+}
+

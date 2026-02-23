49540bd91d35c1d37961932d0f91aba56f48551e
lyanchiik <maksimka031rr@gmail.com>
2026-02-23 13:50:52 +0200

    add my lab1

---
+++ Danylko Maksymilian/lab1/src/main.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main() {
+    cout << "Lab 1: First C++ program";
+    cout << "Variant: 3\n";
+    cout << "Student: Danylko Maksymilian";
+
+    return 0;
+}

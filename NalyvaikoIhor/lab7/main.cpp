e49cffef8ceaf3240940c8cceb79d61bf51e27a2
decodeigor <nalyvaikoigor546@gmail.com~>
2026-03-20 21:48:48 +0200

    chinasess

---
+++ NalyvaikoIhor/lab7/main.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    string s;
+    getline(cin, s);
+
+    string words[100];
+    int count = 0;
+
+    string temp="";
+    for (int i=0; i< s.length(); i++) {
+        if (s[i] ==',' || s[i] == '.'){
+            if (temp[0] == ' '){
+                temp.erase(0,1);
+            }
+            words[count] = temp;
+            count++;
+            temp = "";
+
+        } else {
+            temp += s[i];
+        }
+    }
+    for (int i= count -1; i>=0; i--){
+        cout << words[i];
+        if (i !=0) cout << ", ";
+    }
+
+    cout << "." << endl;
+
+    
+}

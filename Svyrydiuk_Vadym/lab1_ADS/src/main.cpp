4dac5f1d3a11f97b82c60c962d5f48b082b70e07
Vadym@2216@2413 <www.svyrydiuk.vadym@gmail.com>
2026-05-17 01:01:34 +0300

    Add files via upload

---
+++ Svyrydiuk_Vadym/lab1_ADS/src/main.cpp
@@ -0,0 +1,30 @@
+﻿#include <iostream>
+
+using namespace std;
+
+int main() {
+	int x;
+	cout << "Enter x = ";
+	cin >> x;
+	int numbers[1000];
+	for (int i = 0; i < x; i++) {
+		cout << "Enter numbers[i]: ";
+		cin >> numbers[i];
+	}
+	int min = numbers[0];
+	int replacement = 0;
+	int sum = 0;
+	for (int i = 0; i < x; i++) {
+		if (numbers[i] <= min) {
+			min = numbers[i];
+			replacement = i;
+		}
+		sum += numbers[i];
+	}
+
+	numbers[replacement] = sum / x;
+	for (int i = 0; i < x; i++) {
+		cout << numbers[i] << " ";
+	}
+	return 0;
+}

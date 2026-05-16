edb620ae044a97a13f779d3c7161515702396410
Vadym@2216@2413 <www.svyrydiuk.vadym@gmail.com>
2026-05-16 15:02:33 +0300

    Add files via upload

---
+++ Svyrydiuk_Vadym/Programming/lab10/src/main2.cpp
@@ -0,0 +1,37 @@
+﻿#include <iostream>
+#include <string>
+
+using namespace std;
+
+int main() {
+	string s;
+
+	cout << "Enter word: ";
+	cin >> s;
+
+	char *p[100];
+
+	for (int i = 0; i < s.length(); i++) {
+		p[i] = &s[i];
+	}
+
+	char t = 0;
+
+	for (int i = 1; i < s.length() - 2; i += 2) {
+		for (int j = 1; j < s.length() - 2; j += 2) {
+			if (*p[j] > *p[j + 2]) {
+				t = *p[j];
+				*p[j] = *p[j + 2];
+				*p[j + 2] = t;
+			}
+		}
+	}
+
+	cout << "Sorted letters: ";
+
+	for (int i = 1; i < s.length(); i += 2) {
+		cout << *p[i];
+	}
+
+	return 0;
+}

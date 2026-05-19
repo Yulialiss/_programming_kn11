d2d141817da53818d0127b78b99c16378a612518
Vadym@2216@2413 <www.svyrydiuk.vadym@gmail.com>
2026-05-19 16:30:28 +0300

    Add files via upload

---
+++ Svyrydiuk_Vadym/ADS/lab4_ADS/src/main.cpp
@@ -0,0 +1,38 @@
+﻿#include <iostream>
+#include <ctime>
+
+using namespace std;
+
+int main() {
+	int n;
+	cout << "Enter matrix size: ";
+	cin >> n;
+
+	int A[100][100];
+	srand(time(0));
+
+	for (int i = 0; i < n; i++) {
+		for (int j = 0; j < n; j++) {
+			A[i][j] = 10 - rand() % 21;
+			cout << A[i][j] << " ";
+		}
+
+		cout << endl;
+	}
+
+	for (int i = 0; i < n; i++) {
+		for (int j = 0; j < n; j++) {
+			if (A[i][j] < 0) {
+				cout << "First negative: " << A[i][j] << endl;
+				cout << "Row: " << i + 1 << endl;
+				cout << "Column: " << j + 1 << endl;
+
+				return 0;
+			}
+		}
+	}
+
+	cout << "No negative elements";
+
+	return 0;
+}

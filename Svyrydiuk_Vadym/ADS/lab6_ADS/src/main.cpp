971f8845a5016bb25d03b1d6533df4ee083ac6f8
Vadym@2216@2413 <www.svyrydiuk.vadym@gmail.com>
2026-05-22 03:49:37 +0300

    Add files via upload

---
+++ Svyrydiuk_Vadym/ADS/lab6_ADS/src/main.cpp
@@ -0,0 +1,34 @@
+﻿#include <iostream>
+
+using namespace std;
+
+// рекурсивна функція обчислення комбінацій
+int C(int n, int m) {
+
+    // якщо m = 0 або m = n
+    if (m == 0 || m == n) {
+        return 1;
+    }
+
+    // якщо m більше n
+    if (m > n) {
+        return 0;
+    }
+
+    // рекурсивне обчислення
+    return C(n - 1, m - 1) + C(n - 1, m);
+}
+
+int main() {
+
+    int n, m;
+
+    // введення значень
+    cout << "Enter n and m: ";
+    cin >> n >> m;
+
+    // виведення результату
+    cout << "Result: " << C(n, m);
+
+    return 0;
+}

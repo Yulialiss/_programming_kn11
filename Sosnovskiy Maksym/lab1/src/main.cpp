421c767d4579f621f40607dbf0eb0ff46a8183da
SosnovskiyMaksym <nerounhlp@gmail.com>
2026-05-23 13:32:17 +0300

    Create main.cpp

---
+++ Sosnovskiy Maksym/lab1/src/main.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+int main() {
+    cout << "Laboratory Work #1" << endl;
+    cout << "Student: Sosnovskiy Maksym" << endl;
+    cout << "Variant: 13" << endl;
+    cout << "University: Lviv National University of Veterinary Medicine and Biotechnology" << endl;
+    
+    return 0;
+}
